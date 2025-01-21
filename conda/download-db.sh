3fe85ddaefa8060bc80d850cd2e2d5b44e8becdf
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-21 22:12:00 +0100

    Conda build update

--- conda/download-db.sh
+++ conda/download-db.sh
@@ -3,14 +3,14 @@
 echo "Downloading pMLST database to ${PMLST_DB}..."
 
 cd ${PMLST_DB}
-git clone https://bitbucket.org/genomicepidemiology/pmlst_db.git
-mv pmlst_db/* .
+cd ..
 rm -rf pmlst_db/
-pMLST_DB=$(pwd)
+git clone https://bitbucket.org/genomicepidemiology/pmlst_db.git
+cd pmlst_db
+
 # Install pMLST database with executable kma_index program
 python INSTALL.py kma_index
 
 echo "pMLST database is downloaded."
 
 exit 0
-
